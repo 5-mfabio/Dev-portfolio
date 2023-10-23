a91a416f33ad2f0712c5a935c79c3d03e526ef00
5mfabio <96273323+5mfabio@users.noreply.github.com>
2023-10-23 19:36:32 -0300

    quarto commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    // Lógica para enviar o formulário (pode ser adicionada aqui)
+    // Exemplo: Enviar dados para um servidor ou exibir uma mensagem de sucesso.
+    alert("Formulário enviado com sucesso!");
+});
